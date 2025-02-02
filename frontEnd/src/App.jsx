f582e0073c9cb5dc5025d71a21c1cec9e72ee3a3
Nguyen <nguyenmandat000@gmail.com>
2025-02-03 02:44:45 +0700

    start code FE

---
+++ frontEnd/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className='text-red-300'>Hello</h1>
+      <h1 className='text-red-900'>Test</h1>
+    </>
+  )
+}
+
+export default App
