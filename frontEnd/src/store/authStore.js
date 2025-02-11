3544ae8d07f368b9d86f7e823fc405081f7170ef
Mandat0911 <s3877932@rmit.edu.vn>
2025-02-11 22:34:57 +0700

    complete auth

--- frontEnd/src/store/authStore.js
+++ frontEnd/src/store/authStore.js
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import axios from "axios";
-
-const API_URL = import.meta.env.MODE === "development" ? "http://localhost:5002/api/auth" : "/api/auth";
+import API_URL from "../config.js";
 
 axios.defaults.withCredentials = true;
 
