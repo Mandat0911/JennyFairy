e1a52b39c1035c8e0efc1de7ddfd703d8aaea297
Nguyen Man Dat <92921695+Mandat0911@users.noreply.github.com>
2025-03-31 17:09:21 +0700

    Update vite.config.js

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from "@vitejs/plugin-react"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss(), react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5002"
+        target: "http://localhost:5002",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
+  build: {
+    outDir: 'dist', // Ensure the output directory is set correctly
+    sourcemap: true, // Optional: Helps with debugging in production
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor'; // Splitting vendor code
+          }
+        }
       }
     }
   }
