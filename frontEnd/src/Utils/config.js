eaab93b2810d53306e7f8731741edbcecdb93f52
Nguyen Man Dat <92921695+Mandat0911@users.noreply.github.com>
2025-04-04 22:01:10 +0700

    Update config.js

--- frontEnd/src/Utils/config.js
+++ frontEnd/src/Utils/config.js
@@ -1,4 +1,4 @@
-const API_BE_URL = "https://www.api.jennyfairy.store/api";
+const API_BE_URL = "https://api.jennyfairy.store/api";
 
 // Product API Endpoints
 export const PRODUCT_API_ENDPOINTS = {
