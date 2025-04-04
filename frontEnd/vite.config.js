ca49156119b8bbbcf19fd7032ebb37592fb3e866
Nguyen Man Dat <92921695+Mandat0911@users.noreply.github.com>
2025-04-04 17:35:44 +0700

    Update vite.config.js

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://api.jennyfairy.store/api",
+        target: "https://api.jennyfairy.store",
         changeOrigin: true,
         secure: false,
       }
