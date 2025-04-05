af1f73b6a451b1c52c6c6990a3764c08c3a39d0b
Nguyen Man Dat <92921695+Mandat0911@users.noreply.github.com>
2025-04-06 02:20:30 +0700

    Update ProductCardSkeleton.jsx

--- frontEnd/src/Components/Other/ProductCardSkeleton.jsx
+++ frontEnd/src/Components/Other/ProductCardSkeleton.jsx
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const ProductCardSkeleton = () => {
-  return (
-    <div className="w-full bg-white rounded-lg overflow-hidden shadow-md animate-pulse">
-      {/* Image Skeleton */}
-      <div className="w-full h-60 sm:h-72 md:h-96 bg-gray-200" />
+    return (
+        <div className="relative w-full bg-white rounded-lg overflow-hidden shadow-md animate-pulse">
+            {/* Image Skeleton */}
+            <div className="relative w-full h-60 sm:h-72 md:h-96 bg-gray-200" />
 
-      {/* Content Skeleton */}
-      <div className="p-3 sm:p-4 text-center space-y-2">
-        {/* Title */}
-        <div className="h-4 sm:h-5 bg-gray-200 rounded w-3/4 mx-auto" />
+            {/* Text Skeletons */}
+            <div className="p-3 sm:p-4 text-center">
+                {/* Name placeholder */}
+                <div className="h-4 sm:h-5 w-3/4 bg-gray-200 mx-auto rounded mb-3" />
 
-        {/* Price */}
-        <div className="flex justify-center gap-2 mt-2">
-          <div className="h-4 w-12 bg-gray-200 rounded" />
-          <div className="h-4 w-12 bg-gray-200 rounded" />
+                {/* Price placeholders */}
+                <div className="flex items-center justify-center gap-2 mt-1">
+                    <div className="h-4 sm:h-5 w-1/4 bg-gray-200 rounded" />
+                    <div className="h-4 sm:h-5 w-1/4 bg-gray-200 rounded" />
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
-}
+    );
+};
 
-export default ProductCardSkeleton
+export default ProductCardSkeleton;
